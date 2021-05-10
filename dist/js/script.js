1b94b00e023d9478671118bcd75f01f98d470b8d
paulius147 <76919595+paulius147@users.noreply.github.com>
2021-05-10 13:03:53 +0300

    Update script.js

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,3 +1,5 @@
 document.querySelector('.menu').addEventListener('click', function(){
   document.querySelector('.navbar').classList.toggle('navbar-active');
 });
+
+document.addEventListener("touchstart", function() {}, true);
